26edfcac01eea07c284ed7484ce335183cb0dd3e
Igor Irić <iricigor@gmail.com>
2023-09-14 09:04:52 +0200

    adding Measure Table

---
+++ samples/Measure-KQLTable.ps1
@@ -0,0 +1,54 @@
+function Measure-KQLTable {
+
+[CmdletBinding()]
+param (
+    [string]$table = 'StormEvents',
+    [string]$database = 'https://help.kusto.windows.net/Samples',
+    [string[]]$column = @(),
+    [switch]$NoSamples
+)
+
+if (!$column) {
+    Write-Verbose "Getting column names from $table..."
+    $sample = Invoke-KQL "$table | take 5"
+    $cols = $sample | gm -MemberType NoteProperty
+    $column = $cols.Name
+}
+
+foreach ($c1 in $column) {
+    
+    if ($Column.count -gt 1) {
+        Write-Progress -Activity "Checking columns..." -PercentComplete (100*(++$currentColumnCount)/($column.count)) -Status "Column $c1"
+    } else {
+        Write-Verbose "Checking column $c1..."
+    }
+    $stat = Invoke-KQL "$table | summarize count() by tostring($c1) | sort by count_ desc" -Database $database -Verbose:$false
+    if (!$totalRows) {
+        $totalRows = ($stat | Measure-Object -Property count_ -sum).Sum
+        Write-Verbose "Total rows: $totalRows"
+    }
+    $blank = $stat | where $c1 -eq ''
+    $top5 = [int](($stat[0..4] | Measure-Object -Property count_ -Sum).Sum)
+
+    [PSCustomObject]@{
+        ColumnName = $c1
+        UniqueValues = $stat.Count
+        IsPrimaryKey = ($stat.Count -eq $totalRows)
+        BlankValues = if ($blank) {$blank.count_} else {0}
+        BlankPercentage = if ($blank) {[int](100 * $blank.count_ / $totalRows)} else {0}
+
+        Top1Value = if ($NoSamples) {''} else {$stat[0].$c1}
+        Top1Count = $stat[0].count_
+        Top1Percentage = [int](100 * $stat[0].count_ / $totalRows)
+
+        Top5Values = if ($NoSamples) {''} else {$stat[0..4].$c1 -join ', '}
+        Top5Count = $top5
+        Top5Percentage = [int](100 * $top5 / $totalRows)
+    }
+
+    
+}
+
+    Write-Progress -Completed -Activity 'done'
+
+}
