780d9a578542bcfe5566a6786a486ea008b2ed21
Igor Iric <iricigor@gmail.com>
2024-09-20 19:11:06 +0200

    update to NET8, add Cursor changes

--- InvokeKQL.cs
+++ InvokeKQL.cs
@@ -33,19 +33,38 @@ namespace KQL
         // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
         protected override void ProcessRecord()
         {
-            foreach (string Q1 in Query) {
+            foreach (string Q1 in Query)
+            {
                 WriteVerbose($"Executing query '{Q1}' ...");
-                var reader = client.ExecuteQuery(Q1);
-                WriteVerbose("Parsing query results...");
-                while (reader.Read()) {
-                    PSObject rowData = new PSObject();
-                    Enumerable.Range(0, reader.FieldCount)
-                        .Select(i => new PSNoteProperty(reader.GetName(i), reader.GetValue(i)))
-                        .ToList()
-                        .ForEach(rowData.Properties.Add);
-                    WriteObject(rowData);
-                };
-                WriteVerbose("Query complete!");
+                try
+                {
+                    var reader = client.ExecuteQuery(Q1);
+                    WriteVerbose("Parsing query results...");
+                    
+                    var fieldNames = Enumerable.Range(0, reader.FieldCount)
+                        .Select(i => reader.GetName(i))
+                        .ToArray();
+
+                    while (reader.Read())
+                    {
+                        var rowData = new PSObject();
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            rowData.Properties.Add(new PSNoteProperty(fieldNames[i], reader.GetValue(i)));
+                        }
+                        WriteObject(rowData);
+                    }
+
+                    WriteVerbose("Query complete!");
+                }
+                catch (Kusto.Data.Exceptions.KustoException ex)
+                {
+                    WriteError(new ErrorRecord(ex, "KustoQueryError", ErrorCategory.InvalidOperation, Q1));
+                }
+                catch (System.Exception ex)
+                {
+                    WriteError(new ErrorRecord(ex, "QueryExecutionError", ErrorCategory.InvalidOperation, Q1));
+                }
             }
         }
 
